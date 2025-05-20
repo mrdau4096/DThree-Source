a33a9b2ec56d670a7ba41afce46021b32b218172
Shabbles <oshabley11@gmail.com>
2025-05-20 18:59:35 -0400

    Update responses.py

--- exct/responses.py
+++ exct/responses.py
@@ -10,6 +10,8 @@ import pandas as pd
 import numpy as np
 from exct.shared import sendMessage, replyMessage, getTime, timeSinceStr, secondsSince
 
+kill = False
+
 
 global invalidDates
 previousChoices = {}
